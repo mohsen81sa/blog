fb36395c58fa5d1d5804f7a5d5cf82015f82d751
mohsen <107324736+mohsen81sa@users.noreply.github.com>
2025-02-16 15:58:01 +0330

    cbv blog & detail
    api view detail

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,7 +4,8 @@ app_name = "blog"
 
 urlpatterns = [
     # path("post/",views.post_list,name="post_list"),
-    path("post/",views.PostList.as_view(),name="post_list"),
-    path("post/<int:id>/",views.post_detaile,name="post_detaile"),
+    # path("post/<int:id>/",views.post_detaile,name="post_detaile"),
+    path("post/",views.PostList.as_view(),name="post-list"),
+    path("post/<int:id>/",views.PostDetail.as_view(),name="post-detaile"),
 
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,21 +6,42 @@ from .sreializers import PostSerializer
 from rest_framework.views import APIView
 from ...models import Post
 from django.shortcuts import get_object_or_404
-@api_view(["GET","POST"])
-@permission_classes([IsAuthenticatedOrReadOnly])
-def post_list(request):
-    if request.method == "GET":
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts,many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+
+""" fbv api """
+# @api_view(["GET","POST"])
+# @permission_classes([IsAuthenticatedOrReadOnly])
+# def post_list(request):
+#     if request.method == "GET":
+#         posts = Post.objects.filter(status=True)
+#         serializer = PostSerializer(posts,many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+# @api_view(["GET","PUT","DELETE"])
+# @permission_classes([IsAuthenticated])
+# def post_detaile(request,id):
+#     post = get_object_or_404(Post,pk=id,status=True)
+#     if request.method == "GET":
+#         serializer = PostSerializer(post)    
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = PostSerializer(post,data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == "DELETE":
+#         post.delete()
+#         return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
 
 
+# cbv api
 class PostList(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
     def get(self,request):
         posts = Post.objects.filter(status=True)
         serializer = PostSerializer(posts,many=True)
@@ -31,19 +52,21 @@ class PostList(APIView):
         serializer.save()
         return Response(serializer.data)
     
-@api_view(["GET","PUT","DELETE"])
-@permission_classes([IsAuthenticated])
-def post_detaile(request,id):
-    post = get_object_or_404(Post,pk=id,status=True)
-    if request.method == "GET":
-        serializer = PostSerializer(post)    
+class PostDetail(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    
+    def get(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
+        serializer = self.serializer_class(post)    
         return Response(serializer.data)
-    elif request.method == "PUT":
+    def put(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
         serializer = PostSerializer(post,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == "DELETE":
+    def delete(self,request,id):
+        post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
-
